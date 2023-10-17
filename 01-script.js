a3a10d7faf65ffdfd97e4e4908a6d07b3f2b42df
Derya Gülmez <deryaglmz0@gmail.com>
2023-10-17 16:31:06 +0300

    Javascript

---
+++ 01-script.js
@@ -0,0 +1,23 @@
+console.log("merhaba");
+
+//
+
+console.log(7000 + (7000 * 0.3)) // maaş ali
+console.log(6000 + (6000 * 0.3)) // maaş can
+
+//
+
+var maasAli = 5000;
+var maasCan = 6000;
+var zam = 0.3;
+
+console.log(maasAli + (maasAli * zam)) // maaş ali
+console.log(maasCan + (maasCan * zam)) // maaş can
+
+
+// türkçe karakter kullanımaz
+// arada boşluk olamaz
+// sayı ile başlayamaz ama sonu sayı ile bitirebilir 
+
+//
+
